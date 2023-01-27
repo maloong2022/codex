236497e0d96d4c74ee16b76327eac951c5b2cee2
maloong <maloong2022@gmail.com>
2023-01-27 19:18:50 +0800

    fix: add a organization config

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const configuration = new Configuration({
+  organization: "org-QHRXX8MB79DsGZxTC0JOscbg",
   apiKey: process.env.OPENAI_API_KEY,
 });
 
